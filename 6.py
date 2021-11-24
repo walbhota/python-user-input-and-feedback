4394f4c248004592081a044c0a50da11dbf9b44e
j8 <walbhota@gmail.com>
2021-11-24 01:50:00 +0100

    user input and interaction

---
+++ 6.py
@@ -0,0 +1,11 @@
+print("Welcome to the program, what is your name?")
+name_result = input("Enter your response here ->")
+print(f"Your response was {name_result}")
+
+print("what did you think of the food you ate today?")
+food_result = input("Enter your response here ->")
+print(f"Your response was {food_result}")
+
+print("What tv show ending did you dislike the most ever?")
+show_result = input("Enter your response here ->")
+print(f"Your response was {show_result}")
